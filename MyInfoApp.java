16a33b9330decfe15952a8c89aa7d57def72da82
YoonjiO <yoonjee36@gmail.com>
2021-09-17 12:01:21 +0900

    hello

---
+++ MyInfoApp.java
@@ -0,0 +1,8 @@
+
+public class MyInfoApp {
+	public static void main(String[] args) {
+		System.out.println("Oh Yoonji");
+		System.out.println("21 years old");
+		System.out.println("Department of Computer Enginerring");
+	}
+}
